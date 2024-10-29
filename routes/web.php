119e398f664797695808a19ec4902c2e8e05f1df
Jheorgenes Warlley <jheorgenes@gmail.com>
2024-10-29 15:29:53 -0300

    -- Criando uma rota e um novo controlador para tela de dashboards -- Recriando a tela do dashboard -- Reajustando a tela de navegação

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\MainController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -7,17 +8,13 @@ Route::get('/', function () {
     // Se autenticado, redirecionando para a tela de dashboard
     if(auth()->check()){
         return redirect()->route('dashboard');
-        //se não, redirecionando para a tela de login
-    } else {
-        return redirect()->route('login');
     }
+    //se não, redirecionando para a tela de login
+    return redirect()->route('login');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
 Route::middleware('auth')->group(function () {
+    Route::get('/dashboard', [MainController::class, 'index'])->name('dashboard');
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
